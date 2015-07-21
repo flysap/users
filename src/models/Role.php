5c3d87275d04fc5110cff6219f265549bb93e359
Radu <radu.bordei89@gmail.com>
2015-07-21 11:19:19 +0300

    implements sluggable interface.

--- src/models/Role.php
+++ src/models/Role.php
@@ -4,14 +4,23 @@ namespace Flysap\Users\Models;
 
 use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
 
-class Role extends Model {
+class Role extends Model implements SluggableInterface {
+
+    use SluggableTrait;
 
     public $table = 'roles';
 
     public $timestamps = false;
 
     public $fillable = ['name', 'permissions'];
+
+    protected $sluggable = [
+        'build_from' => 'name',
+        'save_to'    => 'slug',
+    ];
 
     /**
      * Get the list of the users .
